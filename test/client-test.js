70cb45eca784c4579d759584ff3515dca6838158
Guillermo Lopez <guillermolopez@pawadominicana.com>
2017-07-27 14:33:44 -0400

    working with test

--- test/client-test.js
+++ test/client-test.js
@@ -338,3 +338,29 @@ test('Delete position', async t => {
 
   t.deepEqual(response.body, position)
 })
+
+test('Save user', async t => {
+  const client = t.context.client
+  const user = fixtures.getUser()
+
+  nock(options.endpoints.users)
+    .post('/save', user)
+    .reply(201, user)
+
+  const response = await client.saveUser(user)
+
+  t.deepEqual(response.body, user)
+})
+
+test('Get user', async t => {
+  const client = t.context.client
+  const user = fixtures.getUser()
+
+  nock(options.endpoints.users)
+    .get(`/${user.id}`)
+    .reply(200, user)
+
+  const response = await client.getUser(user.id)
+
+  t.deepEqual(response.body, user)
+})
